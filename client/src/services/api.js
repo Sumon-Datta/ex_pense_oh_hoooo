a71d50f6a55a39051b4153b3148c96c126743f96
sumon datta <sumondatta16@gmail.com>
2026-08-01 10:38:13 +0600

    Update production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
-  // baseURL: "http://10.62.233.207:5000/api"
+  baseURL: "https://ex-pense-oh-hoooo.onrender.com/api",
 });
 
 export default api;
